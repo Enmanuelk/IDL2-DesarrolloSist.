c7bf0e8eca284f6106eba48d20dbf6e3cb4c44d0
Enmanuel <i2912731@continental.edu.pe>
2023-12-17 22:21:31 -0500

    Subiendo Tarea Api

---
+++ WebApplication1/Controllers/AutorControllers.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAplication
+
+namespace WebAplication
+{
+    [ApiController]
+
+    [Route("api/autores")]
+    public class AutoresControllers: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresControllers(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult<List<AutoresControllers>>> Get() 
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult<Post (Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChanged
+        }
+    }
+}
---
+++ WebApplication1/Controllers/Entidades/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Controllers.Entidades
+{
+    public class Autor
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
